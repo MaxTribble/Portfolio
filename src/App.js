e022dd917eb80f57e697053f36924e7a5b6c8fc9
Max Tribble <maxtribble@live.com>
2023-04-03 21:11:54 -0600

    fixing routes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <NavBar />
        <Routes>
           <Route path="/Portfolio/" element={<Home />} />
-           <Route path="/Portfolio/projects" element={<Projects />} />
+          <Route path="/Portfolio/projects" element={<Projects />} />
            {/* <Route path="/resume" element={<Resume />} /> */}
            {/* <Route path="*" element={<About to="/"/>} /> */}
          </Routes>
