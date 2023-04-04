f0ef0cfe422727e6a8aa93480d18e353575b96c8
Max Tribble <maxtribble@live.com>
2023-04-03 23:21:07 -0600

    changing routes

--- src/App.js
+++ src/App.js
@@ -6,19 +6,61 @@ import Home from './components/Home'
 import Contact from './components/Contact'
 import Projects from './components/Project'
 import Footer from './components/Footer'
+import "./App.css";
+import { useState } from "react";
+
+
 function App() {
+  const [currentPage, setCurrentPage] = useState('Home')
+  function renderPage(){
+    if(currentPage === "Home") {
+      return <Home setCurrentPage= {setCurrentPage}/>
+    }
+    if(currentPage === "Projects") {
+      return <Projects/>
+    }
+
+  }
   return (
-    <Router>
-        <NavBar />
-       <Routes>
-          <Route path="/Portfolio/" element={<Home />} />
-          <Route path="/Project/" element={<Projects />} />
-           {/* <Route path="/resume" element={<Resume />} /> */}
-           {/* <Route path="*" element={<About to="/"/>} /> */}
-         </Routes>
-         <Footer />
-    </Router>
+    <div>
+      <NavBar setCurrentPage = {setCurrentPage}/>
+        {renderPage()}
+          <Footer />
+        </div>
   );
 }
 
 export default App;
+
+// function App() {
+//   const [currentPage, setCurrentPage] = useState('About')
+//   function renderPage(){
+//     if(currentPage === "About") {
+//       return <About setCurrentPage= {setCurrentPage}/>
+//     }
+//     if(currentPage === "Projects") {
+//       return <Projects/>
+//     }
+//     if(currentPage === "Contact") {
+//       return <Contact/>
+//     }
+//     if(currentPage === "Resume") {
+//       return <Resume />
+//     }
+//   }
+//   return (
+//     <div className="App bg-dark">
+//       <Header setCurrentPage = {setCurrentPage}/>
+//       <main>
+
+//         {renderPage()}
+//         <div>
+//           <Footer />
+//         </div>
+//       </main>
+//     </div>
+//   );
+// }
+
+// export default App;
+
--- src/components/navBar.js
+++ src/components/navBar.js
@@ -3,19 +3,61 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function NavBar() {
+function NavBar(props) {
   return (
     <>
       <Navbar bg="dark" variant="dark">
         <Container >
             <Nav className="me-auto"> 
-            <Nav.Link href="/Portfolio/" style={{padding: "5px 30px", fontSize: "18px"}}>Home</Nav.Link>
-            <Nav.Link href="/Portfolio/Project/" style={{padding: "5px 30px", fontSize: "18px"}}>Projects</Nav.Link>
+            <Nav.Link onClick={()=>{props.setCurrentPage('Home')}} href="#" style={{padding: "5px 30px", fontSize: "18px"}}>Home</Nav.Link>
+            <Nav.Link onClick={()=>{props.setCurrentPage('Projects')}} href="#" style={{padding: "5px 30px", fontSize: "18px"}}>Projects</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
     </>
   );
 }
+
+// export default NavBar;
+
+// import React from "react";
+// import logo from "../images/Number-three.jpg"
+// import '../scss/custom.scss'
+
+// function Header(props) {
+//     return (
+
+//     <nav className="navbar navbar-expand-lg" style= {{backgroundColor:"#008b8b"}}>
+//         <div className="container-fluid">
+
+//             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+//             <span className="navbar-toggler-icon"></span>
+//             </button>
+//             <div className="collapse navbar-collapse" id="navbarNav">
+//             <img src={logo} alt="Bootstrap" width="30" height="30"/>
+//             <div className = "navbar-brand">
+//             <a onClick={()=>{props.setCurrentPage('About')}} className="nav-link active" aria-current="page" href="#">Dylan Cassagnol</a>
+//             </div>
+//             <ul className="navbar-nav">
+//                 <li className="nav-item">
+//                 <a onClick={()=>{props.setCurrentPage('About')}} className="nav-link active" aria-current="page" href="#">About</a>
+//                 </li>
+//                 <li className="nav-item">
+//                 <a onClick={()=>{props.setCurrentPage('Projects')}} className="nav-link" href="#">Work</a>
+//                 </li>
+//                 <li className="nav-item">
+//                 <a onClick={()=>{props.setCurrentPage('Contact')}} className="nav-link" href="#">Contact</a>
+//                 </li>
+//                 <li className="nav-item">
+//                 <a onClick={()=>{props.setCurrentPage('Resume')}} className="nav-link" href="#">Resume</a>
+//                 </li>
+//             </ul>
+
+//             </div>
+//         </div>
+//     </nav>
+// // </header>
+//     )
+// };
 
 export default NavBar;
